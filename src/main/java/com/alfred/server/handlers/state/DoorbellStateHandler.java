7583830a30573e9b20b7849fc7c291db7ba10d45
Kevin <kanzelm4@gmail.com>
2015-09-24 11:38:34 -0500

    cleaned up picture callback method and implemented message sending

--- src/main/java/com/alfred/server/handlers/state/DoorbellStateHandler.java
+++ src/main/java/com/alfred/server/handlers/state/DoorbellStateHandler.java
@@ -1,11 +1,8 @@
 package com.alfred.server.handlers.state;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
-
-import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -15,6 +12,7 @@ import com.alfred.common.datamodel.StateDevice;
 import com.alfred.common.handlers.StateDeviceHandler;
 import com.alfred.common.messages.StateDeviceProtos.StateDeviceMessage;
 import com.alfred.common.messages.StateDeviceProtos.StateDeviceMessage.Builder;
+import com.alfred.common.messages.StateDeviceProtos.StateDeviceMessage.State;
 import com.alfred.server.handlers.hardware.WebCamCallback;
 import com.alfred.server.handlers.hardware.WebCameraThread;
 import com.alfred.server.server.Server;
@@ -29,11 +27,13 @@ public class DoorbellStateHandler implements StateDeviceHandler {
 
     private static final Logger log = LoggerFactory.getLogger(DoorbellStateHandler.class);
     private Builder messageBuilder = StateDeviceMessage.newBuilder();
+
     @Override
     public void onAddDevice(StateDevice device) {
         log.info("Device added");
         log.info(device.toString());
     }
+
     /**
      * This method starts a thread to capture an image with the webcam. It also
      * starts building the message to send clients. The message is completed and 
@@ -41,12 +41,19 @@ public class DoorbellStateHandler implements StateDeviceHandler {
      */
     @Override
     public void onUpdateDevice(StateDevice device) {
-        // TODO take picture
-        WebCameraThread webCamThread = new WebCameraThread(new TakePictureCallback(device));
-        new Thread(webCamThread).start();
         // Start building message
         messageBuilder.setId(device.getId())
                       .setState(device.getState());
+        
+        // if the state is being set to Active, take a picture
+        if(device.getState() == State.ACTIVE) {
+            WebCameraThread webCamThread = new WebCameraThread(new TakePictureCallback(device));
+            new Thread(webCamThread).start();
+        } else {
+            // if the state is not being set to active, just send the 
+            // state update message
+            sendMessage();
+        }
     }
 
     @Override
@@ -54,7 +61,27 @@ public class DoorbellStateHandler implements StateDeviceHandler {
         log.info("Device removed");
         log.info(device.toString());
     }
-    
+
+    /**
+     * Builds the message that is constructed in the message builder, then
+     * sends the message to each client connected to the server
+     */
+    private void sendMessage() {
+        // build message
+        StateDeviceMessage deviceMessage = messageBuilder.build();
+
+        // Send message to each client
+        log.info("Sending message");
+        log.info(deviceMessage.toString());
+        for(Socket socket : Server.getServerConnections()) {
+            try {
+                deviceMessage.writeTo(socket.getOutputStream());
+            } catch (IOException e) {
+                log.error("Writing to socket failed", e);
+            }
+        }
+    }
+
     /**
      * This method acts as a callback to the webcam thread. The onComplete
      * method is called by the webcam thread after the picture has been taken.
@@ -65,13 +92,13 @@ public class DoorbellStateHandler implements StateDeviceHandler {
     private class TakePictureCallback implements WebCamCallback {
 
         private StateDevice device;
-        
+
         public TakePictureCallback(StateDevice device) {
             this.device = device;
         }
-        
+
         /**
-         * This method finishes building the message that was started in the parent
+         * This method adds the image to the message that was started in the parent
          * class onDeviceUpdate method. After the message is built it is sent
          * to each client connected to the server
          */
@@ -85,23 +112,7 @@ public class DoorbellStateHandler implements StateDeviceHandler {
             } catch (IOException e1) {
                 log.error("Unable to read image file" + image.getAbsolutePath(), e1);
             }
-            
-            // build message
-            StateDeviceMessage deviceMessage = messageBuilder.build();
-            
-            // Send message to each client
-            log.info("Sending message");
-            log.info(deviceMessage.toString());
-            for(Socket socket : Server.getServerConnections()) {
-                try {
-                    deviceMessage.writeTo(socket.getOutputStream());
-                } catch (IOException e) {
-                    log.error("Writing to socket failed", e);
-                }
-            }
-            
+            sendMessage();
         }
-        
     }
-
 }
