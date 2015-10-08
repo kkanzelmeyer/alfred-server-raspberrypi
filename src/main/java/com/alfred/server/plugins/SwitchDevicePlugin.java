dfefdd53f1a4fa98883fc722c7ff5c509bbeedf5
Kevin Kanzelmeyer <kanzelm4@gmail.com>
2015-10-07 23:27:22 -0500

    created empty plugin for switch controlled devices (lights, fans)

---
+++ src/main/java/com/alfred/server/plugins/SwitchDevicePlugin.java
@@ -0,0 +1,17 @@
+package com.alfred.server.plugins;
+
+public class SwitchDevicePlugin implements DevicePlugin {
+
+    @Override
+    public void activate() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deactivate() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
